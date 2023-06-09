4b5b6428b6699b17da38bd52c871a96cd9fb32d4
ZilishZulfugarli <2002zilis@gmail.com>
2023-06-10 01:53:50 +0400

    Add project files.

---
+++ GeometryHelper/GeometryHelper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryHelper
+{
+    public static class GeometryHelper
+    {
+        public static double Base {get;set;}
+        public static double Height { get;set;}
+        public static void Quadrilateral(double Base, double Height)
+        {
+            double result = (Base * Height) / 2;
+            Console.WriteLine("Area is: " + result);
+        }
+
+        public static double A { get;set;}
+        public static double B { get;set;}
+        public static double C { get;set;}
+        public static void Triangel(double A, double B, double C)
+        {
+            double result = (A + B + C) / 2;
+            Console.WriteLine("Area is: " + result);
+        }
+        public static double R {get; set;}
+        public static void Circle(double R)
+        {
+
+            double result = Math.PI * R * R ;
+            Console.WriteLine("Area is: " + result);
+        }
+    }
+}
---
+++ GeometryHelper/Program.cs
@@ -0,0 +1,15 @@
+﻿
+namespace GeometryHelper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GeometryHelper.Triangel(10, 10, 10);
+
+            GeometryHelper.Quadrilateral(10, 20);
+
+            GeometryHelper.Circle(2.5);
+        }
+    }
+}
